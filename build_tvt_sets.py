875c74e4c953217e16eb8ac918f0a713727b74c0
Ralwoss <patrick_buehler_94@web.de>
2021-04-18 20:03:03 +0200

    added comments, improved readability, changed format

--- build_tvt_sets.py
+++ build_tvt_sets.py
@@ -6,10 +6,11 @@ TRAINCHORMS = ['chr5', 'chr8', 'chr9', 'chr10', 'chr11', 'chr12', 'chr13', 'chr1
 VALCHROMS = ['chr1', 'chr3', 'chr7']
 TESTCHROMS = ['chr2', 'chr4', 'chr6']
 
+#split data in training, validation and testset, still hardcoded split of chromosomes
 def build_tvt(verbose=False):
     # load data
     data, labels = load.load_data()
-    # construct test set
+    # construct training set
     xtrain = []
     ytrain = []
     for chr in TRAINCHORMS:
--- load_data.py
+++ load_data.py
@@ -5,14 +5,3 @@ def load_data(interaction_matrices, labels, verbose = False):
     a = pick.load(open(interaction_matrices, 'rb'))
     b = pick.load(open(labels, 'rb'))
     return a,b
-#
-# data, labels = load_data()
-# x = 0
-# for key in data.keys():
-#     x += len(data[key])
-#     print(key + ": " + str(len(data[key])))
-#
-# print("sum: " + str(x))
-# print("train: " + str(x*0.6)) # rest
-# print("validation: " + str(x*0.2)) # 1, 3, 7
-# print("test: " + str(x*0.2)) # 2, 4, 6
