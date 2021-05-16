df59b699792c7272b7233e336a161089c04d2003
Ralwoss <patrick_buehler_94@web.de>
2021-05-16 17:12:59 +0200

    changed from continuos windows to continuos windows for non boundaries and additional window for every boundary

--- prepare_data.py
+++ prepare_data.py
@@ -42,7 +42,7 @@ def prepare_data(filepath_hicmatrix, filepath_TAD_domains, write_windows = False
     #load hicmatrix
     c = cool.Cooler(filepath_cool)
     numberbins = [] # list of number of bins for each chromosome
-    arr = c.matrix(balance=False)
+    hic_matrix = c.matrix(balance=False)
 
 
 
@@ -53,7 +53,9 @@ def prepare_data(filepath_hicmatrix, filepath_TAD_domains, write_windows = False
 
 
     #dictionaries for submats/boundaries (key:chromosome, value:list of submats/boundaries)
-    submats = {}
+
+    submats_pos = {}
+    submats_neg = {}
     boundaries = {}
     #extract boundaries from TAD_domains.bed
     with open(filepath_bound, 'r') as f:
@@ -64,7 +66,7 @@ def prepare_data(filepath_hicmatrix, filepath_TAD_domains, write_windows = False
             boundaries[cont[0]].append((int(cont[2]) / binsize))
 
     chrcount = 0
-    labels = {}
+    #labels = {}
     #go through every chromosome and compute submatrices
     for number in numberbins:
         chrom = c.chromnames[chrcount]
@@ -76,12 +78,37 @@ def prepare_data(filepath_hicmatrix, filepath_TAD_domains, write_windows = False
             chrcount+=1
             continue
 
-        labels[chrom] = []
+
+
+
+
+
+        #labels[chrom] = []
+        submat = []
         #new start and end position depending on last positions, window_size and overlap sizes
         start = offset
         end = start + window_size
-        submat = []
         startboundary = 0
+
+        #for saving submats of negative and positive windows
+        submat_neg = []
+        submat_pos = []
+
+        # constructs window for every boundary with boundary in the center (positive windows)
+        for bound in chrbounds:
+            if (bound < window_size / 2):
+                continue
+            start = int(bound - int(window_size / 2))
+            end = int(bound + round(window_size / 2))
+            submat_pos.append(hic_matrix[start:end, start:end])
+            if (write_windows):
+                windows.write(chrom + "\t" + str((start)*binsize) + "\t" + str((end-1)*binsize) + "\n")
+                found_boundaries.write(chrom + "\t" + str(int(bound-1)*binsize) + "\t" + str(int(bound+1)*binsize) + "\n")
+
+        start = offset
+        end = start + window_size
+
+
         #go trough hicmatrix and build submatrix with corresponding boundaries flag
         while end <= number + offset:
 
@@ -95,34 +122,36 @@ def prepare_data(filepath_hicmatrix, filepath_TAD_domains, write_windows = False
                 except:
                     break
             label = 0
-            searchboundary = startboundary
-            while(chrbounds[searchboundary] >= start - offset and chrbounds[searchboundary] <= end -offset):
-            #checks if the next boundary is in the current window center and set labels
-                if start - offset + int((end-start)/2) == chrbounds[startboundary]:
+
+
+            #TODO: Überarbeiten
+
+            #checks if boundary in center area detected
+            for bound in boundaries[chrom][startboundary:]:
+                if bound > end - offset:
+                    break
+                centersize = 2 * parameters.detection_range + 1
+                if bound >=  start + int((window_size - centersize)/2) and bound < end - round((window_size-centersize)/2):
                     label = 1
-                    if(write_windows):
-                        found_boundaries.write(chrom+"\t"+str((start - offset + int((end-start)/2)-1)*binsize) + "\t" + str((start - offset + int((end-start)/2)+1)*binsize) + "\n")
+                if label == 1:
                     break
-                try:
-                    chrbounds[searchboundary+1]
-                    searchboundary +=1
-                except:
-                    break
-            submat.append(arr[start:end, start:end])
-            labels[chrom].append(label)
-            if (write_windows):
-                windows.write(chrom + "\t" + str((start - offset)*binsize) + "\t" + str((end - offset)*binsize) + "\n")
+            if(label != 1):
+                submat_neg.append(hic_matrix[start:end, start:end])
+                if (write_windows):
+                    windows.write(chrom + "\t" + str((start - offset)*binsize) + "\t" + str((end - offset)*binsize) + "\n")
 
             start, end = start + window_size - overlap, end + window_size - overlap
         offset = offset + number #computes new offset for chromosome
-        submats[chrom] = submat
+        submats_pos[chrom] = submat_pos
+        submats_neg[chrom] = submat_neg
         chrcount += 1
         print()
 
 
     #save interaction matrices and corresponding labels
-    pick.dump(submats, open('preparations/'+ parameters.save_preparation_id +'/InteractionMatrices' , 'wb'))
-    pick.dump(labels, open('preparations/' + parameters.save_preparation_id + '/labels', 'wb'))
+    pick.dump(submats_pos, open('preparations/'+ parameters.save_preparation_id +'/InteractionMatricesPos' , 'wb'))
+    pick.dump(submats_neg, open('preparations/' + parameters.save_preparation_id + '/InteractionMatricesNeg', 'wb'))
+    #pick.dump(labels, open('preparations/' + parameters.save_preparation_id + '/labels', 'wb'))
     if(write_windows):
         windows.close()
         found_boundaries.close()
