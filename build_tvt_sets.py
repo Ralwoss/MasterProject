da7759b3f9b3c3f524f730b4c251658a740e10bb
Ralwoss <patrick_buehler_94@web.de>
2021-08-09 16:13:30 +0200

    restructuring of build_tvt_sets.py finished

--- build_tvt_sets.py
+++ build_tvt_sets.py
@@ -19,7 +19,7 @@ def build_training_set (cooler,windows_file = parameters.windows_bed, chroms = p
         for line in f:
             count += 1
             if count % 1000 == 0:
-                print(f"{count} Lines of parsed")
+                print(f"{count} Lines parsed")
             cont = line.strip().split()
             if cont[0] not in chroms:
                 continue
@@ -28,6 +28,51 @@ def build_training_set (cooler,windows_file = parameters.windows_bed, chroms = p
             x.append(matrix[offset+(int(cont[1])//binsize):offset+(int(cont[2])//binsize),
                      offset+(int(cont[1])//binsize):offset+(int(cont[2])//binsize)])
             y.append(int(cont[4])//1000)
+            x, y = np.array(x), np.array(y)
+
+    return x,y
+
+def build_validation_set (cooler,windows_file = parameters.windows_bed, chroms = parameters.VALCHROMS):
+    x = []
+    y = []
+    matrix = cooler.matrix(balance=False)
+    with open(windows_file, 'r') as f:
+        count = 0
+        for line in f:
+            count += 1
+            if count % 1000 == 0:
+                print(f"{count} Lines parsed")
+            cont = line.strip().split()
+            if cont[0] not in chroms:
+                continue
+            offset = cooler.offset(cont[0])
+            binsize = cooler.binsize
+            x.append(matrix[offset+(int(cont[1])//binsize):offset+(int(cont[2])//binsize),
+                     offset+(int(cont[1])//binsize):offset+(int(cont[2])//binsize)])
+            y.append(int(cont[4])//1000)
+            x, y = np.array(x), np.array(y)
+
+    return x,y
+
+def build_test_set (cooler,windows_file = parameters.windows_bed, chroms = parameters.TESTCHROMS):
+    x = []
+    y = []
+    matrix = cooler.matrix(balance=False)
+    with open(windows_file, 'r') as f:
+        count = 0
+        for line in f:
+            count += 1
+            if count % 1000 == 0:
+                print(f"{count} Lines parsed")
+            cont = line.strip().split()
+            if cont[0] not in chroms:
+                continue
+            offset = cooler.offset(cont[0])
+            binsize = cooler.binsize
+            x.append(matrix[offset+(int(cont[1])//binsize):offset+(int(cont[2])//binsize),
+                     offset+(int(cont[1])//binsize):offset+(int(cont[2])//binsize)])
+            y.append(int(cont[4])//1000)
+            x, y = np.array(x), np.array(y)
 
     return x,y
 
@@ -38,45 +83,25 @@ def build_training_set (cooler,windows_file = parameters.windows_bed, chroms = p
 
 
 # split data in training, validation and testset, still hardcoded split of chromosomes
-def build_tvt(verbose=False):
+def build_tvt(cooler,windows_file = parameters.windows_bed, trainchroms = parameters.TRAINCHORMS,
+              valchroms = parameters.VALCHROMS, testchroms = parameters.TESTCHROMS, verbose=False):
     # load data
     interaction_matrices_pos, interaction_matrices_neg = load.load_data(parameters.interaction_matrices_pos, parameters.interaction_matrices_neg)
     # construct training set
-    xtrain = []
-    ytrain = []
-    for chr in TRAINCHORMS:
-        xtrain = xtrain + interaction_matrices_pos[chr]+interaction_matrices_neg[chr]
-        ytrain = ytrain + len(interaction_matrices_pos[chr]) * [1] + len(interaction_matrices_neg[chr]) * [0]
-    xtrain = np.array(xtrain)
-    ytrain = np.array(ytrain)
+    xtrain, ytrain = build_training_set(cooler,windows_file, trainchroms)
     if verbose:
         zeros = ytrain[ytrain == 0]
         print("Percent zeros train: " + str(len(zeros) / len(ytrain)))
 
     # construct validation set
-    xval = []
-    yval = []
-    for chr in VALCHROMS:
-        xval = xval + interaction_matrices_pos[chr] + interaction_matrices_neg[chr]
-        yval = yval + len(interaction_matrices_pos[chr]) * [1] + len(interaction_matrices_neg[chr]) * [0]
-    xval = np.array(xval)
-    yval = np.array(yval)
+    xval, yval = build_validation_set(cooler,windows_file,valchroms)
 
     if verbose:
         zeros = yval[yval == 0]
         print("Percent zeros validation: " + str(len(zeros) / len(yval)))
 
     # construct test set
-    xtest = []
-    ytest = []
-    for chr in TESTCHROMS:
-        xtest = xtest + interaction_matrices_pos[chr] + interaction_matrices_neg[chr]
-        ytest = ytest + len(interaction_matrices_pos[chr]) * [1] + len(interaction_matrices_neg[chr]) * [0]
-    xtest = np.array(xtest)
-    ytest = np.array(ytest)
-
-    if(verbose):
-        print(xtrain[0][9])
+    xtest, ytest = build_test_set(cooler,windows_file,testchroms)
 
     if verbose:
         zeros = ytest[ytest == 0]
